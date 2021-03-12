db4e83a116c2947e66676f1f31dc6d833a35120c
Daniel <dominicdj37@gmail.com>
2021-03-12 21:32:33 +0530

    added some error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
     def render_500(error_code = nil, error_message = nil)
         error_code = 'internal_server_error' if error_code.nil?
         respond_to do |format|
-            format.json { render_json_error(500, error_code, error_message) }
+            format.json { render_json_error(500, error_code, error_message, nil) }
         end
     end
 
@@ -32,29 +32,42 @@ class ApplicationController < ActionController::Base
     def render_400(error_code = nil, error_message = nil)
         error_code = 'bad_request' if error_code.nil?
         respond_to do |format|
-            format.json { render_json_error(400, error_code, error_message) }
+            format.json { render_json_error(400, error_code, error_message, nil) }
         end
     end
 
     def render_401(error_code = nil, error_message)
-        error_code = 'unauthorized_access' if error_code.nil?
-        respond_to do |format|
-          format.json { render_json_error(401, error_code, error_message) }
-        end
+      error_code = 'unauthorized_access' if error_code.nil?
+      respond_to do |format|
+        format.json { render_json_error(401, error_code, error_message, nil) }
       end
+    end
 
-      def render_403(error_code = nil, error_message = nil)
-        error_code = 'forbidden' if error_code.nil?
-        respond_to do |format|
-          format.json { render_json_error(403, error_code, error_message) }
-        end
+    def render_403(error_code, error_message = nil, options = {})
+
+      error_code = 'forbidden' if error_code.nil?
+      error_message = "You do not have permission to access this!" if error_message.nil?
+      error_code_params = options && options.is_a?(Hash) && options[:error_code_params] ? options[:error_code_params] : nil
+
+      respond_to do |format|
+        format.json { render_json_error(403, error_code, error_message, error_code_params) }
       end
+    end
 
-    def render_json_error(status_code, error_code, error_message = nil)
+    def render_422(error_code = nil, error_message)
+      error_code = 'unprocessable_entity' if error_code.nil?
+      respond_to do |format|
+       format.json { render_json_error(422, error_code, error_message, nil) }
+      end
+    end
+
+    def render_json_error(status_code, error_code, error_message = nil, error_code_params )
+
       response = {
         error: {
           code: error_code,
-          message: error_message
+          message: error_message,
+          error_code_params: error_code_params
         }
       }
   
--- app/controllers/v1/authentication_controller.rb
+++ app/controllers/v1/authentication_controller.rb
@@ -9,14 +9,14 @@ class V1::AuthenticationController < ApplicationController
     def create 
        
         @user = User.find_by(username: params.require(:username))
-        render_403(error_message: "Invalid username") and return unless @user
+        render_403(nil, "Invalid username", { error_code_params: { error_type: "invalid_username" } }) and return unless @user
 
         if @user.authenticate(params.require(:password))
             token = AuthenticationTokenService.call(@user.id)
             render_200(returnUserModel())
         else 
             # raise AuthenticationError
-            render_403(error_message: "Password do not match")
+            render_403(nil, "Password do not match", { error_code_params: { error_type: "invalid_password" } })
             return
         end
   
@@ -25,7 +25,7 @@ class V1::AuthenticationController < ApplicationController
     private
 
     def parameter_missing(e)
-        render json: { error: e.message }, status: :unprocessable_entity
+        render_422(nil, "Parameter(s) missing")
     end 
 
     def handle_unauthenticated
